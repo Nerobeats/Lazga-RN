0548b70d4795a6bcbfb0d4274e3a4c8ad4ed3c86
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-04-14 20:51:09 +0300

    Authentication

--- redux/index.js
+++ redux/index.js
@@ -1,11 +1,16 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { fetchItems } from './actions/item'
+import { fetchItems } from "./actions/item";
+import { checkForToken } from "./actions/user";
 // Actions
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-store.dispatch(fetchItems())
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+store.dispatch(fetchItems());
+store.dispatch(checkForToken());
 export default store;
